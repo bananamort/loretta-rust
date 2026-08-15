97a6890e62d870fb132b366267fe32a0382b6650
bananamort <302587819+bananamort@users.noreply.github.com>
2026-08-15 15:16:30 -0400

    port: RenamingRewriter + RenameTable -> loretta/src/experimental/minifying/renamingrewriter.rs (#523)
    Co-authored-by: loretta-rs <loretta-rs@local>

--- loretta-rs/loretta/src/experimental/minifying/mod.rs
+++ loretta-rs/loretta/src/experimental/minifying/mod.rs
@@ -4,6 +4,7 @@ pub mod islotallocator;
 pub mod minifyingutils;
 pub mod namingstrategies;
 pub mod namingstrategy;
+pub mod renamingrewriter;
 pub mod sequentialslotallocator;
 pub mod sortedslotallocator;
 pub mod triviarewriter;
